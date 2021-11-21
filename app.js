6fe06861c6f22c08a41c40e1b43ddded1d3c9d95
Olivier Ishimwe <olivierish@gmail.com>
2021-11-22 00:36:22 +0100

    fixed some bugs and initialized the app.js file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//navbar & burger menu handling
 function toggleClassName() {
   const sidebarElt = document.querySelector(".sidebar");
   const openElt = document.getElementById("open");
@@ -14,8 +15,42 @@ function toggleClassName() {
   }
 }
 
+//Pomodoro
+/*
+.affichage{
+  .bloc .travail{
+    p(txt: travail)
+    p.affichageT
+  }
+  .block .pause{
+    p(txt:repos)
+    p.affichageP
+  }
+}
+.container-btns{
+  .btn b1(txt:commencer)
+  .btn b2(txt:Pause)
+  .btn b3(txt:reset)
+}
+*/
+const roundsElt = document.querySelector(".rounds");
+const timeElt = document.querySelector(".time");
+const btnStartElt = document.querySelector(".start");
+const btnPauseElt = document.createElement("button");
+btnPauseElt.setAttribute("class", "pause");
+btnPauseElt.textContent = "Pause";
+const btnResetElt = document.querySelector(".reset");
+
+console.log(roundsElt, timeElt, btnStartElt, btnResetElt, btnPauseElt);
+
+let startTime = 1800; //30min
+let breakTime = 300; //5min
+let isBreak = false;
+let roundsNbr = 0;
+roundsElt.textContent = `Pomodoro ${roundsNbr}`;
+
 window.onload = () => {
-    //window.scrollTo(0, document.body.scrollHeight);
-    document.querySelector(".mask").scrollIntoView({ behavior: "smooth" });
+  //window.scrollTo(0, document.body.scrollHeight);
+  document.querySelector(".mask").scrollIntoView({ behavior: "smooth" });
   document.querySelector(".mask").scrollTop = document.scrollHeight + 0;
 };
